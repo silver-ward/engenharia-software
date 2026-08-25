fb59026b19cd01e702971d95ffa942051e4d8a72
silver-ward <1159936+silver-ward@users.noreply.github.com>
2026-08-24 22:48:02 -0300

    add UA 3 aula 5

---
+++ js/jogoDaVelha.js
@@ -0,0 +1,103 @@
+let tabuleiro;
+let board;
+let aviso;
+let jogador;
+let linha;
+let coluna;
+
+function iniciar(){
+    console.log('Iniciar');
+    tabuleiro = Array(3);
+    board = document.getElementById('board');
+    aviso = document.getElementById('aviso');
+    jogador = 1;
+
+    for(let i = 0; i < 3; i++){
+        tabuleiro[i] = [];
+        for(let j = 0; j < 3; j++){
+            tabuleiro[i][j] = 0;
+        }
+    }    
+    console.table(tabuleiro);
+    exibir();
+}
+
+function exibir(){
+    console.log('Exibir');
+    let tabela = '<table cellpadding="10" border="1">';
+
+    for(let i = 0; i < 3; i++){
+        tabela += '<tr>';
+        for (let j = 0; j < 3; j++){
+
+            switch(tabuleiro[i][j]){
+                case -1: marcador = 'X'; break;
+                case 1: marcador = 'O'; break;
+                default: marcador = '';
+            }
+            
+            tabela += `<td>${marcador}</td>`;
+        }
+        tabela += '</tr>';
+    }
+    tabela += '</table>';
+
+    board.innerHTML = tabela;
+}
+
+function jogar(){
+    console.log('Jogar');
+    aviso.innerHTML = `Vez do jogador ${numeroJogador()}`;
+
+    linha = document.getElementById('linha').value -1;
+    coluna = document.getElementById('coluna').value -1;
+
+    if(tabuleiro[linha][coluna] == 0){
+        tabuleiro[linha][coluna] = numeroJogador() == 1 ? 1 : -1;
+        jogador++;
+    } else{
+        aviso.innerHTML += "<br>Esse campo já foi marcado."
+    }
+    
+    console.table(tabuleiro);
+    exibir();
+    checar();
+}
+
+function checar(){
+
+    // Linhas
+    for(let i = 0; i < 3; i++){
+        let soma = 0;
+        soma = tabuleiro[i][0] + tabuleiro[i][1] + tabuleiro[i][2];
+
+        if(soma == 3 || soma == -3){
+            aviso.innerHTML = `O jogador ${numeroJogador()} ganhou.`
+        }
+    }
+
+    // Colunas
+    for(let i = 0; i < 3; i++){
+        let soma = 0;
+        soma = tabuleiro[0][i] + tabuleiro[1][i] + tabuleiro[2][i];
+
+        if(soma == 3 || soma == -3){
+            aviso.innerHTML = `O jogador ${numeroJogador()} ganhou.`
+        }
+    }
+
+    // Diagonais
+    let soma = tabuleiro[0][0] + tabuleiro[1][1] + tabuleiro[2][2];
+    if(soma == 3 || soma == -3){
+        aviso.innerHTML = `O jogador ${numeroJogador()} ganhou.`
+    }
+
+    soma = tabuleiro[2][0] + tabuleiro[1][1] + tabuleiro[0][2];
+    if(soma == 3 || soma == -3){
+        aviso.innerHTML = `O jogador ${numeroJogador()} ganhou.`
+    }
+}
+
+function numeroJogador(){
+    return jogador%2 + 1;
+}
